9044330a514a830034bbb12ee21e1ee2d0582e59
khazarglzd <khazarguluzade@gmail.com>
2024-11-08 00:12:45 +0400

    adding states for CofeeForm

--- src/components/CofeeForm.jsx
+++ src/components/CofeeForm.jsx
@@ -1,6 +1,18 @@
 import { coffeeOptions } from "../utils"
+import { useState } from "react"
 
 const CofeeForm = () => {
+
+    const [selectedCoffee, setSelectedCoffee] = useState(null)
+    const [showCoffeeTypes, setShowCoffeeTypes] = useState(false)
+    const [coffeeCost, setCoffeeCost] = useState(0)
+    const [hour, setHour] = useState(0)
+    const [min, setMin] = useState(0)
+
+    function handleSubmitForm(params) {
+        console.log(selectedCoffee, coffeeCost, hour, min)
+    }
+
     return (
         <>
             <div className="section-header">
@@ -11,35 +23,48 @@ const CofeeForm = () => {
             <div className="coffee-grid">
                 {coffeeOptions.slice(0, 5).map((option, optionIndex) => {
                     return (
-                        <button className="button-card" key={optionIndex}>
+                        <button onClick={() => {
+                            setSelectedCoffee(option.name)
+                            setShowCoffeeTypes(false)
+                        }} className={"button-card " + (option.name === selectedCoffee ? ' coffee-button-selected' : ' ')} key={optionIndex}>
                             <h4>{option.name}</h4>
                             <p>{option.caffeine} mg</p>
                         </button>
                     )
                 })}
-                <button className="button-card ">
+                <button onClick={() => {
+                    setShowCoffeeTypes(true)
+                    setSelectedCoffee(null)
+                }} className={"button-card " + (showCoffeeTypes ? ' coffee-button-selected' : ' ')}>
                     <h4>Other</h4>
                     <p>n/a</p>
                 </button>
             </div>
-
-            <select id="coffee-list" name="coffee-list">
-                <option value={null}>Select type</option>
-                {coffeeOptions.map((option, optionIndex) => {
-                    return (
-                        <option value={option.name} key={optionIndex}>
-                            {option.name} ({option.caffeine}mg)
-                        </option>
-                    )
-                })}
-            </select>
+            {showCoffeeTypes && (
+                <select onChange={(e) => {
+                    setSelectedCoffee(e.target.value)
+                }} id="coffee-list" name="coffee-list">
+                    <option value={null}>Select type</option>
+                    {coffeeOptions.map((option, optionIndex) => {
+                        return (
+                            <option value={option.name} key={optionIndex}>
+                                {option.name} ({option.caffeine}mg)
+                            </option>
+                        )
+                    })}
+                </select>
+            )}
             <h4>Add the cost ($)</h4>
-            <input className="w-full" type="number" placeholder="4.50" />
+            <input className="w-full" type="number" value={coffeeCost} onChange={(e) => {
+                setCoffeeCost(e.target.value)
+            }} placeholder="4.50" />
             <h4>Time since consumption</h4>
             <div className="time-entry">
                 <div>
                     <h6>Hours</h6>
-                    <select id="hours-select">
+                    <select onChange={(e) => {
+                        setHour(e.target.value)
+                    }} id="hours-select">
                         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map((hour, hourIndex) => {
                             return (
                                 <option key={hourIndex} value={hour}>{hour}</option>
@@ -49,7 +74,9 @@ const CofeeForm = () => {
                 </div>
                 <div>
                     <h6>Mins</h6>
-                    <select id="mins-select">
+                    <select onChange={(e) => {
+                        setMin(e.target.value)
+                    }} id="mins-select">
                         {[0, 5, 10, 15, 30, 45].map((min, minIndex) => {
                             return (
                                 <option key={minIndex} value={min}>{min}</option>
@@ -58,7 +85,7 @@ const CofeeForm = () => {
                     </select>
                 </div>
             </div>
-            <button>
+            <button onClick={handleSubmitForm}>
                 <p>Add Entry</p>
             </button>
         </>
