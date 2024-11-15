fb042e5554fe27cefa56fcd91ed5af22c4bedf03
khazarglzd <khazarguluzade@gmail.com>
2024-11-16 00:31:10 +0400

    creating Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import ReactDom from 'react-dom'
 
-const Modal = () => {
-    return (
-        <div>Modal</div>
+export default function Modal(props) {
+    const { children, handleCloseModal } = props
+
+    return ReactDom.createPortal(
+        <div className='modal-container'>
+            <button onClick={handleCloseModal} className='modal-underlay' />
+            <div className='modal-content'>
+                {children}
+            </div>
+        </div>,
+        document.getElementById('portal')
     )
 }
-
-export default Modal
