a20849d222b421383c0bb2075c57aad0e0828eea
khazarglzd <khazarguluzade@gmail.com>
2024-11-20 20:35:00 +0400

    making Authentication component dynamic

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import { useState } from 'react'
+export default function Authentication(props) {
+    const { handleCloseModal } = props
+    const [isRegistration, setIsRegistration] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isAuthenticating, setIsAuthenticating] = useState(false)
+    const [error, setError] = useState(null)
 
-const Authentication = () => {
+
+
+    async function handleAuthenticate() {
+        if (!email || !email.includes('@') || !password || password.length < 6 || isAuthenticating) { return }
+        try {
+            setIsAuthenticating(true)
+            setError(null)
+
+            if (isRegistration) {
+                // register a user
+                await signup(email, password)
+            } else {
+                // login a user
+                await login(email, password)
+            }
+            handleCloseModal()
+        } catch (err) {
+            console.log(err.message)
+            setError(err.message)
+        } finally {
+            setIsAuthenticating(false)
+        }
+
+    }
+
     return (
         <>
-            <h2 className="sign-up-text">Sign Up / Login</h2>
-            <p> Sign in to your account!</p>
-            <input placeholder="Email" />
-            <input placeholder="********" type="password" />
-            <button ><p>Submit</p></button>
+            <h2 className="sign-up-text">{isRegistration ? 'Sign Up' : 'Login'}</h2>
+            <p>{isRegistration ? 'Create an account!' : 'Sign in to your account!'}</p>
+            {error && (
+                <p>❌ {error}</p>
+            )}
+            <input value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" />
+            <input value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder="********" type="password" />
+            <button onClick={handleAuthenticate}><p>{isAuthenticating ? 'Authenticating...' : 'Submit'}</p></button>
             <hr />
             <div className="register-content">
-                <p> Do not have an account?'</p>
-                <button><p>Sign up</p></button>
+                <p>{isRegistration ? 'Already have an account?' : 'Don\'t have an account?'}</p>
+                <button onClick={() => { setIsRegistration(!isRegistration) }}><p>{isRegistration ? 'Sign in' : 'Sign up'}</p></button>
             </div>
         </>
     )
 }
-
-export default Authentication
